539a5861759984c8c5587b6a0b8a2551e8d29ddc
latikeshris <108168583+latikeshris@users.noreply.github.com>
2022-11-01 16:04:40 +0530

    Update copy.sh

--- dockermt/ScriptFiles/copy.sh
+++ dockermt/ScriptFiles/copy.sh
@@ -6,6 +6,6 @@ PRJ_NAME=gitclone
 GIT_USER=admin123
 GIT_PASS=admin123
 GIT_URL_FROM=http://13.126.217.36:31633/admin123/test1.git
-GIT_URL_TO=http://13.126.217.36:31633/admin123/test2.git
+GIT_URL_TO=http://13.126.217.36:31633/admin123/test3.git
 docker build .
 echo IMAGE_NAME=$GIT_URL_TO
